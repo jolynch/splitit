64565e6fd25428f47701d54df2c84f27cd89c853
Tom Dooner <tom@causes.com>
2013-07-20 15:59:35 -0700

    Add SQLAlchemy
    It's super ugly code, and everything is in one big file, but this is the
    basic setup of the auction/participant system. It's going to go
    something like:
    
    - User A starts an "auction" and inputs the names of the participants
      and item(s) to bid on
    - Each participant goes to http://site/auctions/:auction_id
    - Once there, they click on their name, and then they can bid on said
      item(s)
    - The host can check back for bids later, somehow...

--- web.py
+++ web.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from functools import wraps
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/db.sqlite3"
+db = SQLAlchemy(app)
 
 @app.route('/')
 def homepage():
@@ -7,12 +12,47 @@ def homepage():
 
 @app.route('/step/1', methods=['GET'])
 def step1():
-    return render_template('step1.html')
+    if session['auction_id'] == None:
+        auction = Auction()
+        db.session.add(auction)
+        db.session.commit()
+        session['auction_id'] = auction.id
+
+    return render_template('step1.html', auction_id=session['auction_id'])
+
+def ensure_auction(f):
+    @wraps(f)
+    def tomato(*args, **kwargs):
+        if 'auction_id' not in session:
+            return redirect(url_for('step1'))
+        return f(*args, **kwargs)
+    return tomato
 
 @app.route('/step/1', methods=['POST'])
+@ensure_auction
 def step1_process():
-    return "the people are: " + str(request.form.getlist('people[]'))
+    for name in request.form.getlist('people[]'):
+        p = Participant(name, session['auction_id'])
+        db.session.add(p)
+    db.session.commit()
+    return str(db.session.query(Participant).all())
+
+class Auction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Participant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    auction_id = db.Column(db.Integer, db.ForeignKey('auction.id'))
+    auction = db.relationship('Auction', backref=db.backref('participants', lazy='dynamic'))
+
+    def __init__(self, name, auction_id):
+        self.name = name
+        self.auction_id = int(auction_id)
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = 'IShouldntTellAnyoneThisString'
+    db.create_all()
     app.run()
