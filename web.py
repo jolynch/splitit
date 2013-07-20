3e71b850f405942078b739604f14a799da23b88f
Tom Dooner <tom@causes.com>
2013-07-20 14:50:24 -0700

    Add the beginning of a Web UI

--- web.py
+++ web.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'housing auction'
+    return render_template('homepage.html')
+
+@app.route('/step/1', methods=['GET'])
+def step1():
+    return render_template('step1.html')
+
+@app.route('/step/1', methods=['POST'])
+def step1_process():
+    return "the people are: " + str(request.form.getlist('people[]'))
 
 if __name__ == '__main__':
     app.debug = True
